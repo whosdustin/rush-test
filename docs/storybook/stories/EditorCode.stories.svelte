af0918a652717f763a4bebcd69fa477450c5bea4
Dustin Delatore <dustin@delatore.me>
2021-07-18 13:28:09 -0400

    a lot of work was done

---
+++ docs/storybook/stories/EditorCode.stories.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import { EditorCode } from 'svelte-ui';
+</script>
+
+<Meta 
+  title="Core/EditorCode"
+  component={EditorCode}
+/>
+
+<Template>
+  <EditorCode />
+</Template>
+
+<Story
+  name="Default"
+/>
---
+++ tools/ui/src/editor-code.svelte
@@ -0,0 +1,5 @@
+<script>
+  export let something = "Hello";
+</script>
+
+<h2>{something} World.</h2>
