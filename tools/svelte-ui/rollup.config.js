00ba034bfc8e7f7e5eca89b5874287983f36a883
Dustin Delatore <dustin@delatore.me>
2021-08-17 23:14:33 -0400

    i dunno

--- tools/svelte-ui/rollup.config.js
+++ tools/svelte-ui/rollup.config.js
@@ -56,7 +56,7 @@ export default {
 		css({ output: 'svelte-ui.css' }),
 		json(),
 		resolve({
-      browser: true,
+      browser: false,
       dedupe: ["svelte"],
     }),
 		commonjs(),
--- tools/svelte-ui/src/utils/helpers.js
+++ tools/svelte-ui/src/utils/helpers.js
@@ -1,3 +1,13 @@
+// import { join, append } from 'ramda';
+
+// const _be = (form, ...str) => join('-', [form, ...str])
+// export const _is = (...str) => list => append(_be('is', ...str), list)
+// export const _has = (...str) => list => append(_be('has', ...str), list)
+// export const _are = (...str) => list => append(_be('are', ...str), list)
+
+
+
+
 const is = (str, memo = []) => be(str, 'is', memo)
 const has = (str, modify, memo = []) => be(str, 'has', modify, memo)
 const are = (str, memo = []) => be(str, 'are', memo)
@@ -10,8 +20,10 @@ const be = (str, ...args) => {
   memo.push(string_it(str, ...args))
   return forms_of_be(memo)
 }
+
 const done = (memo, join = ' ') => memo.filter(word => word != null).join(join)
 const string_it = (str, ...modify) => str ? done([...modify ,str], '-') : null
+
 const forms_of_be = (memo) => ({
   has: (str, modify) => has(str, modify, memo),
   is: (str) => is(str, memo),
@@ -19,6 +31,4 @@ const forms_of_be = (memo) => ({
   done: () => done(memo)
 })
 
-export {
-  is, has, are
-}
+export { is, has, are }
